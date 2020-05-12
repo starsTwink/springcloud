493cefd0d71d49ff6a5eb379119b890e04916458
xingxiaochi <2357299623@qq.com>
2020-05-12 14:35:53 +0800

    Eureka服务

--- cloud-eureka-server7002/src/main/java/com/ghx/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/ghx/springcloud/EurekaMain7002.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer//Eureka服务
 public class EurekaMain7002 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7002.class,args);
