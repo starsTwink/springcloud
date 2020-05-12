bcc970f78e43a2e8ae8377889a5067a994352acb
xingxiaochi <2357299623@qq.com>
2020-05-12 14:44:19 +0800

    分布式配置

---
+++ cloud-config-center-3344/src/test/java/com/ghx/springcloud/ConfigCenter3344Tests.java
@@ -0,0 +1,13 @@
+package com.ghx.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCenter3344Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
