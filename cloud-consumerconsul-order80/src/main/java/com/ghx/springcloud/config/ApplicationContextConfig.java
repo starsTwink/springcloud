acc52d1bdfd1715867475010c267035de3ed9836
xingxiaochi <2357299623@qq.com>
2020-05-12 14:34:29 +0800

    服务消费者

--- cloud-consumerconsul-order80/src/main/java/com/ghx/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumerconsul-order80/src/main/java/com/ghx/springcloud/config/ApplicationContextConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationContextConfig {
+
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate(){
