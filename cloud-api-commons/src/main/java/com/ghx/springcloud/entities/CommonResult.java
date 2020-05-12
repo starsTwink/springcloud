c4d4a6677bfebe6b4d34359677ec18488ce36624
xingxiaochi <2357299623@qq.com>
2020-05-12 14:39:25 +0800

    公共模块

--- cloud-api-commons/src/main/java/com/ghx/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/ghx/springcloud/entities/CommonResult.java
@@ -4,8 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data   //不用写Getter,Setter,equals,canEqual,hasCode,toString等方法了
-@AllArgsConstructor  //使用后添加一个构造函数，该构造函数含有所有已声明字段属性参数
-@NoArgsConstructor   //使用后创建一个无参构造函数
+@AllArgsConstructor   //使用后添加一个构造函数，该构造函数含有所有已声明字段属性参数
+@NoArgsConstructor    //使用后创建一个无参构造函数
 public class CommonResult<T> {
 
     private Integer code;
