7fca17f72bac0b32aa65621cab47aea6c5670a4c
xingxiaochi <2357299623@qq.com>
2020-05-12 14:48:31 +0800

    消息驱动-消费者

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/springcloud/StreamMQmMain8803.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：ghx
+ * @date：2020/5/11 11:02
+ * @describe：
+ */
+@SpringBootApplication
+public class StreamMQmMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQmMain8803.class,args);
+    }
+}
