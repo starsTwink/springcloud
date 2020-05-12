02171e6d22f4de6b0059dcca170592fdf53a78cc
xingxiaochi <2357299623@qq.com>
2020-05-12 14:44:49 +0800

    分布式配置

---
+++ cloud-config-client-3355/src/main/java/com/ghx/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.ghx.springcloud.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author：ghx
+ * @date：2020/5/8 15:37
+ * @describe：将配置中心以REST接口的形式暴露，然后客户端访问
+ */
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;  //要访问的3344上的信息
+
+    @GetMapping("/configInfo")	//请求地址
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
